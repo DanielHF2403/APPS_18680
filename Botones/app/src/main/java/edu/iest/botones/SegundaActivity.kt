6d1906e6bffbbeedd4beffe1b76cdea9de9eba21
DanielHF2403 <eduardo.hernandez03@iest.edu.mx>
2023-02-01 18:55:20 -0600

    LinearLayout

--- Botones/app/src/main/java/edu/iest/botones/SegundaActivity.kt
+++ Botones/app/src/main/java/edu/iest/botones/SegundaActivity.kt
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 
 class SegundaActivity : AppCompatActivity() {
+    private var cambioIcon : Boolean = false
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +20,30 @@ class SegundaActivity : AppCompatActivity() {
         Log.d("Datos", "Datos recibidos con exito fueron $fraseRecibida y edad $edad")
 
         var bnAbrirPantalla = findViewById<Button>(R.id.bnPantalla)
+        var img = findViewById<ImageView>(R.id.ivIest)
+        //var img_2 = false
+        img.setOnClickListener {
+            var arrayIcon = arrayOf<Int>(R.drawable.iest, R.drawable.a)
+            if (cambioIcon) {
+                img.setImageResource(arrayIcon[0])
+            } else {
+                img.setImageResource(arrayIcon[1])
+            }
+            cambioIcon = !cambioIcon
+
+            /*if (img_2 == false) {
+                img.setImageResource(R.drawable.a)
+                img_2 = true
+            } else {
+                img.setImageResource(R.drawable.iest)
+                img_2 = false
+            }*/
+
+        }
         bnAbrirPantalla.setOnClickListener {
             val i = Intent(this,MainActivity::class.java)
             startActivity(i)
+            finish()
         }
 
         var bnCerrar = findViewById<Button>(R.id.bnCerrar)
