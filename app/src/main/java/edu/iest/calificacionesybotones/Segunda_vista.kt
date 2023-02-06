6eb8f7fb68a7cd5563750ff864e7b984b5e67310
DanielHF2403 <eduardo.hernandez03@iest.edu.mx>
2023-02-06 16:08:37 -0600

    Recien creado

---
+++ app/src/main/java/edu/iest/calificacionesybotones/Segunda_vista.kt
@@ -0,0 +1,11 @@
+package edu.iest.calificacionesybotones
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Segunda_vista : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda_vista)
+    }
+}
