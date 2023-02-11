4144b6a635e41827d8792fc00ec67c4f4fec52da
DanielHF2403 <eduardo.hernandez03@iest.edu.mx>
2023-02-11 16:18:16 -0600

    Solo aprueba los numeros 7,8 y 9

--- app/src/main/java/edu/iest/calificacionesybotones/MainActivity.kt
+++ app/src/main/java/edu/iest/calificacionesybotones/MainActivity.kt
@@ -1,11 +1,35 @@
 package edu.iest.calificacionesybotones
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etCalif: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etCalif = findViewById(R.id.etCalif)
+        var bnEnviar = findViewById<Button>(R.id.bnenviar)
+        bnEnviar.setOnClickListener{
+            if (obtenerCalif() <= "6.99"){
+                val s = Intent(this, Tercera_vista::class.java).putExtra("Calificaión", obtenerCalif())
+                startActivity(s)
+                finish()
+            } else {
+                val s1 = Intent(this, Segunda_vista::class.java).putExtra("Calificación", obtenerCalif())
+                startActivity(s1)
+                finish()
+            }
+        }
     }
+
+    private fun obtenerCalif() : String{
+        return etCalif.text.toString()
+    }
+
 }
--- app/src/main/java/edu/iest/calificacionesybotones/Tercera_vista.kt
+++ app/src/main/java/edu/iest/calificacionesybotones/Tercera_vista.kt
@@ -1,25 +1,21 @@
 package edu.iest.calificacionesybotones
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 
-class Segunda_vista : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+class Tercera_vista : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_segunda_vista)
+        setContentView(R.layout.activity_tercera_vista)
 
         var califRecibida = intent.getStringExtra("Calificación")
-        val tvCalifA = findViewById<TextView>(R.id.tvAprobado)
-        tvCalifA.text = "Felicidades aprobaste con $califRecibida"
+        val tvCalifR = findViewById<TextView>(R.id.tvReprobado)
+        tvCalifR.text = "Obtuviste un $califRecibida"
 
-        var bnRegresar = findViewById<Button>(R.id.bnRegresar)
+        var bnRegresar = findViewById<Button>(R.id.bnRegresar1)
         bnRegresar.setOnClickListener{
             var s = Intent(this, MainActivity::class.java)
             startActivity(s)
